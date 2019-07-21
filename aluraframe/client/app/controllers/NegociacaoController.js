79cd0d3c106d4deecc8972dae5c61bbf845aa8ff
leopeli <leopeli.it@gmail.com>
2019-07-21 14:54:39 -0300

    criado o controller da negociacao

--- aluraframe/client/app/controllers/NegociacaoController.js
+++ aluraframe/client/app/controllers/NegociacaoController.js
@@ -1,7 +1,26 @@
 class NegociacaoController {
 
+    constructor() {
+
+        //quando você declara o query Selector no metodo constructor passa aa não precisar percorrer o Dom todas as vezes que incluir um nova negociação pois no momento que instancia a classe é como mantivesse o monitoramento ao elemento não precisando assim busca-lo a todo momento
+        this._inputData = document.querySelector('#data')
+        this._inputQuantidade = document.querySelector('#quantidade')
+        this._inputValor = document.querySelector('#valor')
+    }
+
     adiciona(event) {
         event.preventDefault();
-        alert('funcionou!!!')
+        
+        let data = new Date(...this._inputData.value
+            .split('-')
+            .map((item, index) => item - index % 2 )
+        )
+        
+        let negociacao = new Negociacao (
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+        console.log(negociacao)
     }
 }
